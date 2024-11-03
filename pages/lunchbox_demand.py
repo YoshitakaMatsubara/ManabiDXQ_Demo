862485da9387e1bc7e7082a66c8795904568690e
keiYM <160373037+YoshitakaMatsubara@users.noreply.github.com>
2024-11-03 09:49:03 +0900

    Add files via upload

--- pages/lunchbox_demand.py
+++ pages/lunchbox_demand.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.header("お弁当の需要予測")
+st.header("(作成中)お弁当の需要予測")
 st.write("https://signate.jp/competitions/24")
--- pages/price_prediction.py
+++ pages/price_prediction.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.header("民泊の宿泊価格予測")
+st.header("(作成中)民泊の宿泊価格予測")
 st.write("https://signate.jp/competitions/266")
