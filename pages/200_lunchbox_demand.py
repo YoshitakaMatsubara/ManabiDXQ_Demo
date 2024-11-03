916f384346d66ae6d1257032cfbd20076d23aede
keiYM <160373037+YoshitakaMatsubara@users.noreply.github.com>
2024-11-03 21:35:23 +0900

    Update 200_lunchbox_demand.py

--- pages/200_lunchbox_demand.py
+++ pages/200_lunchbox_demand.py
@@ -16,34 +16,3 @@ plt.gcf().autofmt_xdate()
 plt.title("y")
 plt.ylabel("y")
 st.pyplot(fig)
-
-##########################################3
-
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-
-# データの作成
-x = np.linspace(0, 10, 100)
-y1 = np.sin(x)
-y2 = np.cos(x)
-
-# 列を2つ作成
-col1, col2, col3 = st.columns(3)
-
-# 1つ目のグラフを表示
-with col1:
-    fig, ax = plt.subplots()
-    ax.plot(x, y1, label="sin(x)")
-    ax.set_title("Sine Wave")
-    st.pyplot(fig)
-
-# 2つ目のグラフを表示
-with col2:
-    fig, ax = plt.subplots()
-    ax.plot(x, y2, label="cos(x)", color="orange")
-    ax.set_title("Cosine Wave")
-    st.pyplot(fig)
-
-with col3:
-    st.write(train_df)
